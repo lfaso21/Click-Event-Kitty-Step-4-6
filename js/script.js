07ca95c70e754144d650acb117fadb3458397976
Leo Faso <lfaso21@gmail.com>
2024-02-28 16:01:47 -0500

    Added Classes and Conditional Statements

--- js/script.js
+++ js/script.js
@@ -3,7 +3,19 @@ var button = document.querySelector (".show-cat");
 var cat = document.querySelector (".cat");
 //console.log(cat);
 button.addEventListener("click", function(){
-    button.innerText = "It's Working!!";
+   // button.innerText = "It's Working!!"; step 4-6
+   // cat.classList.add("show");  step4-6
+   if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add ("disappear");
+    //console.log("Yes");
+}
+   else { 
     cat.classList.add("show");
+    button.innerText = "Shoo, Cat!";
+    button.classList.remove ("disappear");
+    //console.log("No");
+}
 });
 //console.log(button);
